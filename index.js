383a70664cdf0f660db74c41d62ea31fa67e2aaa
willstacking <william.prince@usherbrooke.ca>
2019-01-23 23:52:24 -0500

    Import Project

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/Style'));
+app.use(express.static(__dirname + '/Image'));
+
+//Get landing page
+app.get("/" , function(req, res) {
+        res.render("IndexV2");
+});
+app.get("/Soumission" , function(req, res) {
+        res.render("Soumission");
+});
+app.get("/NosRealisation" , function(req, res) {
+        res.render("NosRealisation");
+});
+//Start the server
+app.listen(3000, process.env.IP, function (x){
+        console.log('Server started');
+});
